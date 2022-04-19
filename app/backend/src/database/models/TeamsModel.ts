a1ff32677cbe9e07e1223b758936764f91835378
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-19 19:29:35 -0300

    feat: create teams model

--- app/backend/src/database/models/TeamsModel.ts
+++ app/backend/src/database/models/TeamsModel.ts
@@ -1,23 +1,33 @@
-import { Model } from 'sequelize';
+import { Model, INTEGER, STRING } from 'sequelize';
 import db from '.';
 // import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // public <campo>!: <tipo>;
+class Teams extends Model {
+  public id!: number;
+
+  public team_name!: string;
 }
 
-Example.init({
-  // ... Campos
+Teams.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  team_name: {
+    type: STRING(50),
+    allowNull: false,
+  },
 }, {
-  // ... Outras configs
   underscored: true,
   sequelize: db,
-  // modelName: 'example',
+  modelName: 'teams',
   timestamps: false,
 });
 
 /**
-  * `Workaround` para aplicar as associations em TS: 
+  * `Workaround` para aplicar as associations em TS:
   * Associations 1:N devem ficar em uma das instâncias de modelo
   * */
 
@@ -27,4 +37,4 @@ Example.init({
 // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
 // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
-export default Example;
+export default Teams;
