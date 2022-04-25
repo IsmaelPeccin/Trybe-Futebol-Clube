59fd326fbbe854111a85764b0016a8cd5f2ef480
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-25 14:52:06 -0300

    fix: convertendo de snake para camelCase

--- app/backend/src/database/models/TeamsModel.ts
+++ app/backend/src/database/models/TeamsModel.ts
@@ -1,11 +1,10 @@
 import { Model, INTEGER, STRING } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 
 class Teams extends Model {
   public id!: number;
 
-  public team_name!: string;
+  public teamName!: string;
 }
 
 Teams.init({
@@ -15,7 +14,7 @@ Teams.init({
     primaryKey: true,
     allowNull: false,
   },
-  team_name: {
+  teamName: {
     type: STRING(50),
     allowNull: false,
   },
@@ -25,16 +24,5 @@ Teams.init({
   modelName: 'teams',
   timestamps: false,
 });
-
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
 export default Teams;
