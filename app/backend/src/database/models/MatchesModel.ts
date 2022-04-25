7ce622f969c8c6d352c8389742f068189fae2a28
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-25 14:52:55 -0300

    feat: criando model para a tabela matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,59 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class Matches extends Model {
+  public id!: number;
+
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamGoals: number;
+
+  public inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'homeTeams' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'awayTeams' });
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Matches;
