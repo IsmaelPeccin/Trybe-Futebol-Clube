bddca837fa6422cc7c016ac0436c87900c52c971
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-25 14:53:21 -0300

    feat: model para tabela users

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,46 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public id!: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
