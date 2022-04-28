e5473ffbdcd54e5716428cc87c4b6af88d9b5aaa
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-28 16:54:11 -0300

    feat: controller para teams com função para listar

---
+++ app/backend/src/controller/TeamsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { ITeamsService } from '../interfaces';
+
+export default class TeamsController {
+  constructor(private teamsService: ITeamsService) {}
+
+  listTeamsController = async (
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+  )
+  : Promise<void | Response> => {
+    try {
+      const response = await this.teamsService.listTeams();
+
+      return res.status(200).json(response);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
