34f4bbee15c4f6a685497b909d324e02c122866a
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-28 19:50:21 -0300

    refactor: usando função compareSync

--- app/backend/src/service/UserService.ts
+++ app/backend/src/service/UserService.ts
@@ -1,4 +1,4 @@
-import { compare } from 'bcryptjs';
+import { compareSync } from 'bcryptjs';
 import { IUserInfo, ILoginResponse } from '../interfaces/user';
 import User from '../database/models/UsersModel';
 import AuthMiddleware from '../middlewares/Auth';
@@ -13,7 +13,7 @@ export default class UserService {
   public async login(userData: IUserInfo): Promise<ILoginResponse | boolean> {
     const response = await this._userModel.findOne({ where: { email: userData.email } });
 
-    if (!response || !await compare(userData.password, response.password)) {
+    if (!response || !compareSync(userData.password, response.password)) {
       return false;
     }
     const genToken = AuthMiddleware.generateToken(response);
